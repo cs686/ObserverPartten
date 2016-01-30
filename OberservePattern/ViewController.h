7049db55a5c26e9aedcf81dc70ee67840274ea61
cs686 <cs686@126.com>
2016-01-30 10:26:34 +0800

    Initial Commit

---
+++ OberservePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OberservePattern
+//
+//  Created by cq on 16/1/30.
+//  Copyright © 2016年 songsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OberservePattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OberservePattern
+//
+//  Created by cq on 16/1/30.
+//  Copyright © 2016年 songsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
